99e2b30e2bee13e695c6d22ba086a2f4fbde3282
EyZex <eyzex@hotmail.fr>
2020-04-18 19:53:22 +0200

    ajout extension Restaurateur

---
+++ resto-star/src/Doctrine/CurrentRestaurantOwnerExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Restaurants;
+use App\Repository\RestaurantsRepository;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+final class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private $security;
+    private $repository;
+
+    public function __construct(Security $security, RestaurantsRepository $restaurantsRepository)
+    {
+        $this->security = $security;
+        $this->repository = $restaurantsRepository;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Restaurants::class !== $resourceClass || !$this->security->isGranted('ROLE_RESTAURATEUR') || null === $user = $this->security->getUser()) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.user = :current_user', $rootAlias));
+        $queryBuilder->setParameter('current_user', $user->getId());
+    }
+}
